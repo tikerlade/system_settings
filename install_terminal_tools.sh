149f8708acc5e5081037ffc15c997c923d86377c
Ivan <tikerlade@gmail.com>
2022-08-12 16:07:00 +0300

    Removed some sudo

--- install_terminal_tools.sh
+++ install_terminal_tools.sh
@@ -4,10 +4,10 @@
 sudo apt install zsh -y
 
 # Set zsh as default
-sudo chsh -s $(which zsh)
+chsh -s $(which zsh)
 
 # INSTALL TMUX
 sudo apt install tmux -y
 
 # Install oh-my-zsh (https://github.com/ohmyzsh/ohmyzsh#unattended-install)
-sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
