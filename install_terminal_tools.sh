aa7f0c541a609a6b11a73292f21a582a8b0253fd
Ivan <tikerlade@gmail.com>
2022-08-12 14:19:21 +0300

    Specified unattended installation for oh-my-zsh.

--- install_terminal_tools.sh
+++ install_terminal_tools.sh
@@ -3,12 +3,11 @@
 # ISNTALL ZSH
 sudo apt install zsh -y
 
-# Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 # Set zsh as default
 sudo chsh -s $(which zsh)
 
-
 # INSTALL TMUX
 sudo apt install tmux -y
+
+# Install oh-my-zsh (https://github.com/ohmyzsh/ohmyzsh#unattended-install)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
